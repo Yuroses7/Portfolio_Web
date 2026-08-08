d351f13004d6b385a446dec42bb69b88c8432226
Yuroses7 <chayanan23175@gmail.com>
2026-08-08 17:57:24 +0700

    fix profile import in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import { profile } from '../data/portfolioData'
 const links = [
   { href: '#about', label: 'About' },
   { href: '#skills', label: 'Skills' },
